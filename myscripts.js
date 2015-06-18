3390251f1adc2747a0b88ea2de8261fef832aa09
WENDY GOODWIN <wendygoodwin@mac.com>
2015-06-18 13:06:00 -0700

    javascript stuff one functional item hide

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,21 @@
 //Hide Menu
-$(document).ready(function(){
-	$("#openMenu").hide();
-	$("svg-menu").click(function(){
-		$("article").append(function() {
-			return $("#openMenu").show();
-		});
-	});
+$("#openMenu").hide();
+// //Add button functionality
+// $("hamburger").click(function(){
+// 	$("#openMenu").show();
+// 	$("#openMenu").remove();
+// 	// $(this).remove();
+// });
 
-});
+// jQuery(function($){
+//     	$( '#hamburger').click(function(){
+//     		$('#openMenu').addClass('expand')
+//     		$('#hamburger').addClass('btn-none')
+//     	})
+    	
+//     	 $( '.close-btn' ).click(function(){
+//     		$('#openMenu').removeClass('expand')
+//     		$('#hamburger').removeClass('btn-none')
+//     	})
+//   	})
+//  </script>
