3676abe4509c855c0aa3cadb228ae181522f7a4c
WENDY GOODWIN <wendygoodwin@mac.com>
2015-06-19 13:21:10 -0700

    redoing

--- myscripts.js
+++ myscripts.js
@@ -6,7 +6,5 @@
 //Step1:  Listen for event:  Click
 //Step3:  Listen for event:  Click
 	$('#hamburgerMenu' ).click(function() {
-    		$('#recipeMenuOpen').show('slow', function() {
-    	})
-})
-//Step4:  Apply the recipeMenuClosed styling
+    		$('nav').addClass('recipeMenuOpen');
+    {);
