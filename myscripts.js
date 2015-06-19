01686f529a467d55e2f08452c3bbb18a1371ce79
WENDY GOODWIN <wendygoodwin@mac.com>
2015-06-19 08:29:12 -0700

    catching up local branch

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,12 @@
-//Hide Menu
-$("#recipeMenu span").hide();
-// //When Hamburger Pressed
-$(".svg-menu").click(function() {
-	$("#openMenu").show();
-});
+//PSEUDOCODE//
+//step0:  hide the recipeMenuOpen//
+// $(document).ready(function(){
+
+// }
+//Step1:  Listen for event:  Click
+//Step3:  Listen for event:  Click
+	$('#hamburgerMenu' ).click(function() {
+    		$('#recipeMenuOpen').show('slow', function() {
+    	})
+})
+//Step4:  Apply the recipeMenuClosed styling
