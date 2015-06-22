07224ba290a72b32884fd959c244ea0c93d4980b
WENDY GOODWIN <wendygoodwin@mac.com>
2015-06-22 14:44:24 -0700

    zindex making hamburger visible

--- myscripts.js
+++ myscripts.js
@@ -3,3 +3,9 @@ $('#hamburgerMenu' ).click(function() {
     		$('nav').toggleClass('recipeMenuOpen');
     });
 
+// $(function)(){
+// 	$('nav').hide();
+// 	$('#hamburgerMenu').on('click', function(){
+// 		$('nav, recipeMenuOpen').toggle();
+// 	});
+// });
