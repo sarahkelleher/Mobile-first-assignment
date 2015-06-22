0eab31619676012909858eb05e06c47d1a2542de
WENDY GOODWIN <wendygoodwin@mac.com>
2015-06-21 21:01:37 -0700

    partially working jQuery

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,3 @@
-//PSEUDOCODE//
-//step0:  hide the recipeMenuOpen//
-// $(document).ready(function(){
-
-// }
-//Step1:  Listen for event:  Click
-//Step3:  Listen for event:  Click
-	$('#hamburgerMenu' ).click(function() {
-    		$('nav').addClass('recipeMenuClosed');
-    		$('nav').removeClass('recipeMenuClosed'); 	
-})
-//Step4:  Apply the recipeMenuClosed styling
+$('#hamburgerMenu' ).click(function() {
+    		$('nav').toggleClass('recipeMenuOpen');
+    });
